6be4ec3810128de12b0f1d4b8cc4d63daa82e2a7
hkittylover <Hkittylover_320>
2019-03-14 10:57:55 +0800

    modified hosts

--- plexus_2/settings.py
+++ plexus_2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z3g)o5b(wo!f@yr_hl2a^z8p=u=gf5uhc$j@a*u)d5+h23fxjb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['transpodesire.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['https://transpodesire.azurewebsites.net/', 'transpodesire.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
