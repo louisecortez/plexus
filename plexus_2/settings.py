42e541dff691f6955061e5f38015bc3652e095e2
hkittylover <Hkittylover_320>
2019-03-14 17:07:42 +0800

    added CORS middleware

--- plexus_2/settings.py
+++ plexus_2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z3g)o5b(wo!f@yr_hl2a^z8p=u=gf5uhc$j@a*u)d5+h23fxjb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://transpodesire.azurewebsites.net/', 'transpodesire.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.43.144', 'https://transpodesire.azurewebsites.net/', 'transpodesire.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'admin_console.apps.AdminConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
