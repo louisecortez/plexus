1f71190f050792e3e85298bf7bc1310b38d61a3c
louisecortez <louise_cortez@dlsu.edu.ph>
2019-03-15 16:30:19 +0800

    Frontend initial implementation

--- plexus_2/settings.py
+++ plexus_2/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'plexus',
         'USER': 'postgres',
-        'PASSWORD': 'p@ssword',
+        'PASSWORD': '1234',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -125,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
--- plexus_2/urls.py
+++ plexus_2/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -31,3 +33,6 @@ urlpatterns = [
     path('accounts/login/', views.login_view),
     # path('list/', views.list.as_view())
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
