4b768b176567c90741c133911b581341b8501aa1
louisecortez <louise_cortez@dlsu.edu.ph>
2019-03-20 17:22:33 +0800

    Initial implementation of users html

--- plexus/urls.py
+++ plexus/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('upload/', views.upload),
     path('admin/', admin.site.urls),
     path('datafiles/', views.datafiles.as_view()),
+    path('users/', views.users),
     path('barangays/<int:city>/geo.json', views.BarangayGeojson.as_view()),
     path('barangays/<slug:city>/geo.json', views.BarangayGeojson.as_view()),
     path('amenities/<int:city>/geo.csv', views.AmenityGeojson.as_view()),
