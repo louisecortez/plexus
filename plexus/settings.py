e24521d97c64aff9c43336fbc8360eee4aabcb51
hkittylover <Hkittylover_320>
2019-12-08 04:09:07 +0800

    db config

--- plexus/settings.py
+++ plexus/settings.py
@@ -155,8 +155,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # #  Add configuration for static files storage using whitenoise
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 #
-# import dj_database_url
-# prod_db  =  dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 django_heroku.settings(locals())
